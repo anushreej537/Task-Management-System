2e5cf5f5d28ee258231886ffe23575230c40fcd4
anushree <anushreej537@gmail.com>
2024-09-09 15:58:04 +0530

    create model

--- task_management_sys/task_app/models.py
+++ task_management_sys/task_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Role(models.Model):
     ROLE_CHOICES = [
@@ -7,17 +6,18 @@ class Role(models.Model):
         ('CEO','CEO'),
         ('Team Lead','Team Lead'),
     ]
-    role = models.CharField(max_length = 20,choices=ROLE_CHOICES)
+
+    role = models.CharField(max_length=20,choices=ROLE_CHOICES)
 
     def __str__(self):
         return self.role
-
+    
 class Employee(models.Model):
-    user = models.OneToOneField(User,on_delete = models.CASCADE)
-    email = models.EmailField(max_length = 300)
-    role = models.ForeignKey(Role,on_delete = models.CASCADE)
-    team_leader = models.ForeignKey('self',null = True, blank = True,on_delete=models.SET_NULL)
-
+    empname = models.CharField(max_length=250)
+    email = models.EmailField(max_length=200)
+    role = models.ForeignKey(Role,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
+        return self.empname
+    
+    
--- task_management_sys/task_management_sys/settings.py
+++ task_management_sys/task_management_sys/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--b49$v-#0tz6d1y3y1#r%y)nc6g!ossy4f#!fgaq04epdq5jms'
+SECRET_KEY = 'django-insecure-&n23snmrq*@p11p1rbh%m-x*j$c1nhi0l9lmm&*ltt=+%6wz(8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
