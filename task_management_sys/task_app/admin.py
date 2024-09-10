275bc335455b83eb0267c10fa4409fea0ce040df
anushree <anushreej537@gmail.com>
2024-09-10 11:30:57 +0530

    task model done

--- task_management_sys/task_app/admin.py
+++ task_management_sys/task_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Role,Employee
+from .models import Role,Employee,Project,Task
 
 admin.site.register(Role)
 admin.site.register(Employee)
+admin.site.register(Project)
+admin.site.register(Task)
--- task_management_sys/task_app/models.py
+++ task_management_sys/task_app/models.py
@@ -12,6 +12,7 @@ class Role(models.Model):
     def __str__(self):
         return self.role
     
+    
 class Employee(models.Model):
     empname = models.CharField(max_length=250)
     email = models.EmailField(max_length=200)
@@ -19,5 +20,34 @@ class Employee(models.Model):
 
     def __str__(self):
         return self.empname
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=250)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(null= True,blank=True)
+
+    def __str__(self):
+        return self.project_name
+
+
     
-    
+class Task(models.Model):
+    STATUS_CHOICES = [
+    ('Process', 'Process'),
+    ('Pending', 'Pending'),
+    ('Complete', 'Complete'),
+    ('Testing', 'Testing'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    assigned_by = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, related_name='assigned_tasks') # TL who assigns
+    assigned_to = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='tasks') # Employee working on the task
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.project}"
